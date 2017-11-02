0e9bbdf623e18982f40c3a89c7a7e245c83bec87
Eric Wyne <ecwyne@gmail.com>
2017-11-02 14:02:09 -0700

    copy gramps bin from gramps-graphql/gramps

--- bin/create/create.js
+++ bin/create/create.js
@@ -12,5 +12,12 @@ export const handler = async argv => {
   const plopPath = path.resolve(__dirname, './plopfile.js');
   const plop = nodePlop(plopPath);
   const generator = plop.getGenerator('source');
-  generator.runPrompts(argv.name ? [argv.name] : []).then(generator.runActions);
+  generator
+    .runPrompts(argv.name ? [argv.name] : [])
+    .then(generator.runActions)
+    .then(obj => {
+      obj.changes.forEach(({ path }) => console.log('[SUCCESS]', path));
+      obj.failures.forEach(({ error }) => console.log('[FAIL]', error));
+      console.log(`Created data-source-${argv.name}`);
+    });
 };
