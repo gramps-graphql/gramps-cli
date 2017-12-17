baed2865453873f36f5b5d19898475497fc61843
Jason Lengstorf <jason@lengstorf.com>
2017-12-17 15:40:31 -0600

    feat: dev command ready for review
    - data sources are transpiled by default
    - `--no-transpile` flag is respected
    - a custom gateway can be supplied for dev (#10)
    - temporary files are removed on process exit (#16)
    
    close #11, close #10, close #16

--- bin/gramps.js
+++ bin/gramps.js
@@ -1,21 +1,8 @@
 import { EOL } from 'os';
 import yargs from 'yargs';
-import cleanup from 'node-cleanup';
-
-import { description, version } from '../package.json';
 import dev from './dev';
-// import cleanup from '../lib/cleanup';
 
 yargs
   .command(dev)
-  .demandCommand()
+  // .demandCommand()
   .help().argv;
-
-cleanup(
-  () => {
-    // console.log('exiting...');
-  },
-  {
-    ctrl_C: `${EOL}${EOL}Thanks for using GrAMPS!`,
-  },
-);
