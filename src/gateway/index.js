3330362227d1fcdcc49030f645986a2dabf04d2b
Logan McCaul <loganmccaul@us.ibm.com>
2019-05-02 09:15:56 -0500

    fix: Update default gateway to handle async and prop check for data sources

--- src/gateway/index.js
+++ src/gateway/index.js
@@ -7,7 +7,7 @@ export const GRAPHQL_ENDPOINT = '/graphql';
 export const TESTING_ENDPOINT = '/playground';
 export const DEFAULT_PORT = 8080;
 
-export default config => {
-  const app = configureApp(Express(), config);
+export default async config => {
+  const app = await configureApp(Express(), config);
   startServer(app, config);
 };
